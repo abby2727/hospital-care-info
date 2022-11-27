52602b6a15e0b3e4a6d359fb2620efa54d77a5f3
John Philip T. Estobo <105591828+philipestobo23@users.noreply.github.com>
2022-11-27 23:21:15 +0800

    form-doctor

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -3,67 +3,80 @@
 @section('title', 'Add Patients')
 
 @section('content')
-    <div class="container-fluid px-4">
-        <div class="text-center p-4">
-            <h3 class="text-primary">REGISTER PATIENT</h3>
-        </div>
+<div class="container-fluid px-4">
+    <div class="text-center p-4">
+        <h3 class="text-primary">REGISTER PATIENT</h3>
+    </div>
 
-        <form action="{{ route('patients.store') }}" method="POST">
-            @csrf
-            <div class="row">
-                {{-- <div class="col-3">
+    <form action="{{ route('patients.store') }}" method="POST">
+        @csrf
+        <div class="row d-flex justify-content-center align-content-center ">
+            {{-- <div class="col-3">
                     <img src="https://source.unsplash.com/collection/190727/1000x900" class="d-block img-fluid" alt="image">
                 </div> --}}
-                <div class="col-4">
-                    <div class="input-group mb-3">
-                        <span class="input-group-text bg-primary"><i class="bi bi-person-plus-fill text-white"></i></span>
-                        <select class="form-select" name="doctor_id" aria-label="Default select example">
-                            <option value="" disabled selected>Select Specialist</option>
-                            @foreach ($doctors as $doctor)
-                                <option value="{{ $doctor->id }}">{{ $doctor->first_name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" name="first-name" class="form-control" placeholder="First Name">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" name="middle-name" class="form-control" placeholder="Middle Name">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" name="last-name" class="form-control" placeholder="Last Name">
-                    </div>
-                    <div class="input-group mb-3">
-                        <select class="form-select" name="sex" aria-label="Default select example">
-                            <option value="0">Male</option>
-                            <option value="1">Female</option>
-                        </select>
-                    </div>
+
+            <div class="col-4">
+                <label for="exampleInputEmail1">Gender</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text bg-primary"><i class="bi bi-person-plus-fill text-white"></i></span>
+                    <select class="form-select" name="doctor_id" aria-label="Default select example">
+                        <option value="" disabled selected>Select Specialist</option>
+                        @foreach ($doctors as $doctor)
+                        <option value="{{ $doctor->id }}">{{ $doctor->first_name }}</option>
+                        @endforeach
+                    </select>
                 </div>
-                <div class="col-5">
-                    <div class="input-group mb-3">
-                        <input type="number" name="age" class="form-control" placeholder="Age">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" name="address" class="form-control" placeholder="Address">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="number" name="number" class="form-control" placeholder="Contact Number">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="email" name="email" class="form-control" placeholder="Email">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" name="prescription" class="form-control" placeholder="Presciption">
-                    </div>
+                <label for="exampleInputEmail1">Gender</label>
+                <div class="input-group mb-3">
+                    <input type="text" name="first-name" class="form-control" placeholder="First Name">
                 </div>
-                {{-- <div class="d-grid col-3 mx-auto">
-                    <button class="btn btn-primary mt-4" type="submit"><span></span>Save Record</button>
-                </div> --}}
-                <div class="d-grid">
-                    <button class="btn btn-primary mt-4" type="submit"><span></span>Save Record</button>
+                <label for="exampleInputEmail1">Gender</label>
+                <div class="input-group mb-3">
+                    <input type="text" name="middle-name" class="form-control" placeholder="Middle Name">
+                </div>
+                <label for="exampleInputEmail1">Gender</label>
+                <div class="input-group mb-3">
+                    <input type="text" name="last-name" class="form-control" placeholder="Last Name">
+                </div>
+                <label for="exampleInputEmail1">Gender</label>
+                <div class="input-group mb-3">
+                    <select class="form-select" name="sex" aria-label="Default select example">
+                        <option value="0">Male</option>
+                        <option value="1">Female</option>
+                    </select>
                 </div>
             </div>
-        </form>
-    </div>
+
+            <div class="col-5">
+                <label for="exampleInputEmail1" class="fw-bold">Age</label>
+                <div class="input-group mb-3">
+                    <input type="number" name="age" class="form-control">
+                </div>
+                <label for="exampleInputEmail1" class="fw-bold">Address</label>
+                <div class="input-group mb-3">
+                    <input type="text" name="address" class="form-control">
+                </div>
+                <label for="exampleInputEmail1" class="fw-bold">Contact Number</label>
+                <div class="input-group mb-3">
+                    <input type="number" name="number" class="form-control">
+                </div>
+                <label for="exampleInputEmail1" class="fw-bold">Email Address</label>
+                <div class="input-group mb-3">
+                    <input type="email" name="email" class="form-control ">
+                </div>
+                <label for="exampleInputEmail1" class="fw-bold">Presciption</label>
+                <div class="input-group mb-3">
+                    <input type="text" name="prescription" class="form-control" >
+                </div>
+            </div>
+            {{-- <div class="d-grid col-3 mx-auto">
+                    <button class="btn btn-primary mt-4" type="submit"><span></span>Save Record</button>
+                </div> --}}
+
+        </div>
+        <div class="d-grid col-3 mx-auto">
+            <button class="btn btn-primary mt-4 fw-bold" type="submit"><span></span>Save Record</button>
+        </div>
+    </form>
+</div>
 @endsection
