dfec19439a2819f69eccde84bbca1e29fe1fad9d
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2022-11-27 23:15:04 +0800

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Admin\Doctor;
+use App\Models\Admin\Appointment;
 use App\Models\Admin\Patient;
-use App\Models\Appointment;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
@@ -14,6 +12,7 @@ class DashboardController extends Controller
     {
         $patient = Patient::count();
         $appointment = Appointment::count();
+
         return view('admin.dashboard', compact('patient', 'appointment'));
     }
 }
