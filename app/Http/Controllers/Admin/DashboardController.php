2f92d560cd70ee358a8cad0152251caeacc6fc4b
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2022-11-29 00:45:52 +0800

    Update-admin-dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Appointment;
+use App\Models\Admin\Doctor;
 use App\Models\Admin\Patient;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $patient = Patient::count();
+        $doctors = Doctor::count();
+        $patients = Patient::count();
         $appointment = Appointment::count();
 
-        return view('admin.dashboard', compact('patient', 'appointment'));
+        return view('admin.dashboard', compact('doctors', 'patients', 'appointment'));
     }
 }
