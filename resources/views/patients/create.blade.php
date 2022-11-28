952081e6a0c489af46c0422dfb8f28dfb00ff502
John Philip T. Estobo <105591828+philipestobo23@users.noreply.github.com>
2022-11-28 12:57:30 +0800

    Update create.blade.php

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -65,46 +65,8 @@
                     <div class="input-group mb-3">
                         <input type="text" name="diagnosis" class="form-control" placeholder="Diagnosis">
                     </div>
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Middle Name</label>
-                <div class="input-group mb-3">
-                    <input type="text" name="middle-name" class="form-control" placeholder="Middle Name">
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Last Name</label>
-                <div class="input-group mb-3">
-                    <input type="text" name="last-name" class="form-control" placeholder="Last Name">
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Gender</label>
-                <div class="input-group mb-3">
-                    <select class="form-select" name="sex" aria-label="Default select example">
-                        <option value="0">Male</option>
-                        <option value="1">Female</option>
-                    </select>
-                </div>
-            </div>
 
-            <div class="col-5">
-                <label for="exampleInputEmail1" class="fw-bold">Age</label>
-                <div class="input-group mb-3">
-                    <input type="number" name="age" class="form-control">
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Address</label>
-                <div class="input-group mb-3">
-                    <input type="text" name="address" class="form-control">
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Contact Number</label>
-                <div class="input-group mb-3">
-                    <input type="number" name="number" class="form-control">
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Email Address</label>
-                <div class="input-group mb-3">
-                    <input type="email" name="email" class="form-control ">
-                </div>
-                <label for="exampleInputEmail1" class="fw-bold">Presciption</label>
-                <div class="input-group mb-3">
-                    <input type="text" name="prescription" class="form-control">
-                </div>
-            </div>
+        
             {{-- <div class="d-grid col-3 mx-auto">
                     <button class="btn btn-primary mt-4" type="submit"><span></span>Save Record</button>
                 </div> --}}
