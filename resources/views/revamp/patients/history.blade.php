8ffa159c52bdc21c819e2844aab9c9a530baa997
Christian Dave M. Bate <cbate@ssct.edu.ph>
2022-12-07 04:38:54 -0400

    tagtanggal nakog pula bai

--- resources/views/revamp/patients/history.blade.php
+++ resources/views/revamp/patients/history.blade.php
@@ -19,11 +19,10 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card-body" style="text-align: center;">
-
                 <h4>SCAN YOUR QR CODE</h4>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
                 <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-                <video class="border border-danger border-3 rounded-start " id="preview"></video>
+                <video id="preview"></video>
                 <script type="text/javascript">
                     let scanner = new Instascan.Scanner({
                         video: document.getElementById('preview')
